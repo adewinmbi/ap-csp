012a091c513b219b8941b7b184aeb00074aa3dfb
Brianna Adewinmbi <brianna.adewinmbi@gmail.com>
2021-09-09 09:59:30 -0700

    Complete first robot maze

---
+++ robotmaze.py
@@ -0,0 +1,59 @@
+#   a115_robot_maze.py
+import turtle as trtl
+
+#----- maze and turtle config variables
+screen_h = 400
+screen_w = 420
+startx = -100
+starty = -100
+turtle_scale = 1.5
+
+#------ robot commands
+def move():
+  robot.dot(10)
+  robot.fd(50)
+
+def turn_left():
+  robot.speed(0)
+  robot.lt(90)
+  robot.speed(2)
+
+def turn_right():
+  robot.speed(0)
+  robot.lt(-90)
+  robot.speed(2)
+
+#----- init screen
+wn = trtl.Screen()
+wn.setup(width=screen_w, height=screen_h)
+robot_image = "robot.gif"
+wn.addshape(robot_image)
+
+#----- init robot
+robot = trtl.Turtle(shape=robot_image)
+robot.hideturtle()
+robot.color("darkorchid")
+robot.pencolor("darkorchid")
+robot.penup()
+robot.setheading(90)
+robot.turtlesize(turtle_scale, turtle_scale)
+robot.goto(startx, starty)
+robot.speed(2)
+robot.showturtle()
+
+#---- TODO: change maze here
+wn.bgpic("maze1.png") # other file names should be maze2.png, maze3.png
+
+#---- While loops
+i = 0
+while (i < 2): # Outer while loop
+    n = 0
+    while (n < 4): # Inner while loop
+        move()
+        n += 1
+    turn_right()
+    i += 1
+
+#---- end robot movement 
+
+wn.mainloop()
