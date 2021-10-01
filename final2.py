c8b14ada742f72d4c8e082aaeed7b415bbf1a1cc
Brianna Adewinmbi <brianna.adewinmbi@gmail.com>
2021-09-30 22:05:24 -0700

    Begin boids algoritm implementation

--- final2.py
+++ final2.py
@@ -1,14 +1,24 @@
+# Implementation based on the boids algorithm
+
 import turtle as t
+import random
 painter = t.Turtle()
 
-turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle"]
+turtleShapes = ["arrow", "turtle", "circle", "square", "triangle"]
+turtleColors = ["darkred", "darkblue", "lime", "salmon", "indigo", "brown", "red", "blue", "green", "orange", "purple", "gold"]
 turtles = []
 
-totalTurtles = 20
-startIncrement = 5 # Space between the beginning position of each turtle
+totalTurtles = turtleShapes.count()
 
-while (totalTurtles > 0):
-    totalTurtles -= 1
+spawnRange = 100 # Negative spawnRange defines the lower and leftmost boundaries, while positive spawnRange defines the upper and rightmost boundaries
+
+for s in turtleShapes:
+    newTurtle = t.Turtle(shape = s)
+    turtles.append(newTurtle)
+    turtles.penup()
+    newColor = random.choice(turtleColors)
+    newTurtle.fillcolor(newColor)
+    newTurtle.goto(-spawnRange, spawnRange)
 
 wn = t.Screen()
 wn.mainloop()
