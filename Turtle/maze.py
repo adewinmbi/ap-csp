9e149b545ff5c287304719d1b8e0e47e02a9e270
Brianna Adewinmbi <brianna.adewinmbi@gmail.com>
2021-10-22 09:03:55 -0700

    Update maze.py

--- Turtle/maze.py
+++ Turtle/maze.py
@@ -1,5 +1,6 @@
 # 2D array, True = blocked, false = open
-# Every spot will be initialized as closed
+# Every spot will be initialized as open
+# Using recursive division algorithm
 # Turtle starts at origin
 
 import turtle
@@ -9,11 +10,10 @@ import random
 maze_stamp = turtle.Turtle()
 maze_stamp.shape("square")
 maze_stamp.color("blue")
-maze_stamp.shapesize(10)
+maze_stamp.shapesize(0.1)
 maze_stamp.penup()
 
-grid_size = 500
-num_visited = 0
+grid_size = 100
 grid = []
 
 def neighbors(current, grid):
@@ -26,9 +26,11 @@ def neighbors(current, grid):
   ]
 
   # Remove points out of bounds
-  for i in neighbors_list:
-    if i[0] < 0 or i[0] > grid_size - 1 or grid[i[0]][i[1]]:
-      neighbors_list.remove(current)
+  temp_list = list(neighbors_list)
+
+  for i in temp_list:
+    if (i[0] < 0 or i[0] > len(grid) - 1 or i[1] < 0 or i[1] > len(grid)):
+      neighbors_list.remove(i)
   
   return neighbors_list
 
@@ -43,17 +45,20 @@ def generate_grid():
 
 def generate_maze(): # Aldous-Broder
   x, y = 0, 0
+  num_visited = 0
   grid[x][y][1] = True
 
   while (num_visited < grid_size - 1):
     random_neighbor = random.choice(neighbors((x, y), grid))
     nx, ny = random_neighbor
+    print("visited: ", str(num_visited))
     if (grid[nx][ny][1] == False): # If cell has not been visited
-      grid[nx][ny][1] = True
+      # TODO: Carve passage between new cell and old cell
+      grid[nx][ny][1] = True # Set status of new and old cells to visited
       grid[x][y][1] = True
-      x = nx
-      y = ny
-      print("path made at: " + nx, ny)
+      num_visited += 1
+    x = nx
+    y = ny
 
 def draw_maze(): # Multiply each coord by 5 so each cell is 10 units wide
   for x in range(0, grid_size - 1):
@@ -61,6 +66,7 @@ def draw_maze(): # Multiply each coord by 5 so each cell is 10 units wide
       maze_stamp.goto(x * 5, y * 5)
       if (grid[x][y][0] == True): # If current cell is blocked
         maze_stamp.stamp()
+        print("wall")
 
 generate_grid()
 generate_maze()
