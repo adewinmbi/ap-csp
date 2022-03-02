c0f3f9d059df9b703564c0431538a81b36cc89e6
Brianna Adewinmbi <brianna.adewinmbi@gmail.com>
2022-03-02 13:48:37 -0800

    Add score analysis and reporting

--- PerformanceTask/main.py
+++ PerformanceTask/main.py
@@ -15,8 +15,8 @@ current_letter = ""
 current_score = 0
 round = 0
 
-user_scores = []
-font_size = 52
+score_list = []
+font_size = 55
 # 65 = A, 90 = Z
 
 def new_test():
@@ -24,15 +24,12 @@ def new_test():
     generate_letter()
 
 def end_round():
-    # Save score in text file
-    with open('scores.txt', 'a') as f:
-        f.write(str(current_score))
-        f.write('\n')
-
     # Report score to user
     frame_end.tkraise()
-    # tell user if they got worse, better, have no previous scores, would like to try again, the score they just recieved
-    lbl_results.config(text="You got a " + str(current_score) + "/10!")
+    load_scores()
+    save_score(current_score)
+    analysis = analyze_scores(load_scores(), current_score)
+    lbl_results.config(text="You got a " + str(current_score) + "/10!\n" + analysis, font=("Courier", 10)) # tell user if they got worse, better, have no previous scores, would like to try again, the score they just recieved
 
 def submit_ans():
     global current_score
@@ -40,7 +37,7 @@ def submit_ans():
 
     if (inp_answer.get() == current_letter or inp_answer.get() == current_letter.lower()):
         current_score += 1
-    lbl_letter.config(text=generate_letter())
+    generate_letter()
 
     round += 1
     print(round)
@@ -60,13 +57,38 @@ def generate_letter():
 
     # Decrease size of letter display
     font_size = font_size - 5
-    lbl_letter.config(font=("Courier", font_size))
+    lbl_letter.config(text=letter, font=("Courier", font_size))
 
-    return letter
+# ===== Manage User Scores =====
 
-# ===== Save/Load User Scores =====
+def load_scores():
+    global score_list
 
+    with open('scores.txt', 'r') as f:
+        score_list = f.read().split('\n')
+        score_list.pop()
+        print(score_list)
 
+    return score_list
+
+def save_score(score):
+    with open('scores.txt', 'a') as f:
+        f.write(str(score))
+        f.write('\n')
+
+def analyze_scores(user_scores, new_score):
+    sum = 0
+
+    for score in user_scores:
+        sum += int(score)
+    average = sum / len(user_scores)
+
+    if (new_score < average):
+        print(average)
+        return "Your vision score decreased from your average of " + str(round(average, 1)) + ". Consider visiting an optometrist to improve your eyesight!"
+    else:
+        return "Your vision score improved from your average of " + str(round(average, 1)) + "! Stay healthy!"
+    
 
 # ===== User Interface ===== (maybe reorganize this so everything isnt just a block of texts)
 
