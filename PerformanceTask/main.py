4080236e759d7b06c1fb61a2d23f13bb81f98499
Brianna Adewinmbi <brianna.adewinmbi@gmail.com>
2022-03-02 13:23:10 -0800

    Add score saving into a text file

--- PerformanceTask/main.py
+++ PerformanceTask/main.py
@@ -1,3 +1,4 @@
+from os.path import exists
 import tkinter as tk
 import random
 
@@ -10,22 +11,64 @@ import random
 
 # TODO: Make sure the method I want to be graded is at the top
 
+current_letter = ""
+current_score = 0
+round = 0
+
 user_scores = []
-largest_font = 50
+font_size = 52
 # 65 = A, 90 = Z
 
 def new_test():
     frame_test.tkraise()
+    generate_letter()
 
+def end_round():
+    # Save score in text file
+    with open('scores.txt', 'a') as f:
+        f.write(str(current_score))
+        f.write('\n')
+
+    # Report score to user
+    frame_end.tkraise()
+    # tell user if they got worse, better, have no previous scores, would like to try again, the score they just recieved
+    lbl_results.config(text="You got a " + str(current_score) + "/10!")
+
+def submit_ans():
+    global current_score
+    global round
+
+    if (inp_answer.get() == current_letter or inp_answer.get() == current_letter.lower()):
+        current_score += 1
+    lbl_letter.config(text=generate_letter())
+
+    round += 1
+    print(round)
+    if (round >= 10): # REMOVE/FIX the test lasts 10 rounds
+        round = 0
+        end_round()
+
+    # print(current_score)
+
+def generate_letter():
+    global current_letter
+    global font_size
+
+    # Generate random number and convert to ASCII letter
+    letter = chr(random.randint(65, 90))
+    current_letter = letter
+
+    # Decrease size of letter display
+    font_size = font_size - 5
+    lbl_letter.config(font=("Courier", font_size))
+
+    return letter
 
 # ===== Save/Load User Scores =====
 
-with open('readme.txt', 'w') as f:
-    f.write('readme')
 
 
-
-# ===== User Interface =====
+# ===== User Interface ===== (maybe reorganize this so everything isnt just a block of texts)
 
 # Create main window
 root = tk.Tk()
@@ -38,16 +81,29 @@ frame_main.grid(row=0, column=0, sticky="news")
 frame_test = tk.Frame(root)
 frame_test.grid(row=0, column=0, sticky="news")
 
+frame_end = tk.Frame(root)
+frame_end.grid(row=0, column=0, sticky="news")
+
 # Text labels
 lbl_instructions = tk.Label(frame_main, text="Begin a new test or view old vision scores!", font="Courier")
 lbl_instructions.pack()
 
-lbl_letter = tk.Label(frame_test, text="A", font=("Courier", largest_font))
+lbl_letter = tk.Label(frame_test, text="", font=("Courier", font_size))
 lbl_letter.pack()
+
+lbl_results = tk.Label(frame_end, text="", font=("Courier", font_size))
+lbl_results.pack()
+
+# Input boxes
+inp_answer = tk.Entry(frame_test)
+inp_answer.pack()
 
 # Buttons
 btn_new_test = tk.Button(frame_main, text="New Test", command=new_test)
 btn_new_test.pack()
+
+btn_submit = tk.Button(frame_test, text="Submit Answer", command=submit_ans)
+btn_submit.pack()
 
 frame_main.tkraise()
 root.mainloop()
